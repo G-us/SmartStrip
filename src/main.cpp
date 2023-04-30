9e26c20eeade541d91036c97993b2768e7d80e8c
henryp <henrygparsons@gmail.com>
2023-04-30 14:53:33 +0200

    what is happening

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
+
+#define DECODE_NEC          // Includes Apple and Onkyo
+
+
 #include <Arduino.h>
 
+#include "PinDefinitionsAndMore.h" // Define macros for input and output pin etc.
+#include "IRCommands.h" // Include all IR commands
+#include <IRremote.hpp>
+
+String input;
+char SelectedCommand;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    IrSender.begin(ENABLE_LED_FEEDBACK);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    input = Serial.readStringUntil('\n');
+    Serial.println(input);
+    SelectedCommand = input;
+    IrSender.sendNEC(IRAddress, SelectedCommand, 0);
+}
 }
